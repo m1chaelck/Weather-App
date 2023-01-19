a86064c4865fa2198295bb7368abe9f80a535743
Michael Chamkin <mfixed@gmail.com>
2023-01-19 05:33:22 -0500

    added weather forecast

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 import sys
 import requests
 from flask_sqlalchemy import SQLAlchemy
+import secrets
+import datetime
+
 
 # Flask, SqlAlchemy setup
+secret = secrets.token_urlsafe(32)
 app = Flask(__name__)
+app.secret_key = secret
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
+
+# API Key for OpenWeatherMap (requires free registration)
+API_ID = '6db2423a8fba1d1de7cbc8057328717d'
 
 
 class City(db.Model):
@@ -18,8 +26,11 @@ class City(db.Model):
         return self.name
 
 
-# API Key for OpenWeatherMap (requires free registration)
-API_ID = '6db2423a8fba1d1de7cbc8057328717d'
+def get_city_data(city):
+    response = requests.get(
+        "http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}"
+        "&units=metric".format(city, API_ID)).json()
+    return response
 
 
 @app.route('/')
@@ -27,13 +38,30 @@ def index():
     dict_with_weather_info = {}
     cities = City.query.all()
     for city in cities:
-        response = requests.get(
-            "http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric".format(city, API_ID))
-        name, temp, state, city_id = response.json()['name'], \
-            response.json()['main']['temp'], \
-            response.json()['weather'][0]['main'], \
-            response.json()['id']
-        dict_with_weather_info[name.upper()] = {'temp': round(temp), 'state': state, 'id': city_id}
+        response_curr_weather = get_city_data(city)
+        response_future_weather = get_detailed_city_data(city)
+        if (response_curr_weather['cod'] == '404' or
+                response_future_weather['cod'] == '404'):
+            flash('city cannot be found')
+        name, curr_temp, state, city_id = response_curr_weather['name'], \
+            response_curr_weather['main']['temp'], \
+            response_curr_weather['weather'][0]['main'], \
+            response_curr_weather['id']
+        first_day_temp = round(response_future_weather['list'][7]['main']['temp'])
+        second_day_temp = round(response_future_weather['list'][15]['main']['temp'])
+        third_day_temp = round(response_future_weather['list'][23]['main']['temp'])
+        fourth_day_temp = round(response_future_weather['list'][31]['main']['temp'])
+        fifth_day_temp = round(response_future_weather['list'][39]['main']['temp'])
+        dict_with_weather_info[name.upper()] = {
+            'curr_temp': round(curr_temp),
+            'curr_state': state,
+            'id': city_id,
+            'first_day_temp': first_day_temp,
+            'second_day_temp': second_day_temp,
+            'third_day_temp': third_day_temp,
+            'fourth_day_temp': fourth_day_temp,
+            'fifth_day_temp': fifth_day_temp
+        }
     return render_template('index.html', weather=dict_with_weather_info)
 
 
@@ -41,9 +69,25 @@ def index():
 def add_city():
     if request.method == 'POST':
         city_name = request.form['city_name'].upper()
-        db.session.add(City(name=city_name))
-        db.session.commit()
+        exists = db.session.query(
+            db.session.query(City).filter_by(name=city_name).exists()
+        ).scalar()
+        if not exists:
+            db.session.add(City(name=city_name))
+            db.session.commit()
+            flash("city has been successfully added")
+        flash("cannot add the same city twice")
         return redirect('/')
+
+
+@app.route('/<name>', methods=['GET'])
+def get_detailed_city_data(city):
+    if request.method == 'GET':
+        response = requests.get(
+            "http://api.openweathermap.org/data/2.5/forecast?q={}&APPID={}"
+            "&units=metric".format(city, API_ID)).json()
+        # city_name = response['city']['name']
+        return response
 
 
 @app.route('/delete/<name>', methods=['POST'])
@@ -53,6 +97,7 @@ def remove_city(name):
         db.session.delete(city)
         db.session.commit()
         return redirect('/')
+
 
 # Flask app startup
 if __name__ == '__main__':
